19ce96b236bee34ca62c5ec781267d283baa9519
Anders Westin <64782763+awestin67@users.noreply.github.com>
2026-03-19 17:02:59 +0100

    Release 0.0.3

--- custom_components/battery_optimizer_light_plus/const.py
+++ custom_components/battery_optimizer_light_plus/const.py
@@ -15,7 +15,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # const.py
-DOMAIN = "battery_optimizer_light_base"
+DOMAIN = "battery_optimizer_light_plus"
 
 # Batterityper
 CONF_BATTERY_TYPE = "battery_type"
